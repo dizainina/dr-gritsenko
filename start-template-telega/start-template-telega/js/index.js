bdbd6a07f372e82a5ea3342e96587c581e67d655
dizainina <125885853+dizainina@users.noreply.github.com>
2023-12-08 04:40:51 +0300

    Updates

--- start-template-telega/start-template-telega/js/index.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-//==========================================
-// const TELEGRAM_BOT_TOKEN = '';
-// const TELEGRAM_CHAT_ID = '';
-// const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`
